fe47709ca26fa79f43157da2ec99e5ef6b34c780
mxaddict <mxaddict@codedmaster.com>
2023-04-13 10:51:06 +0800

    Updated the fish config and install script

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,16 +1,17 @@
+#!/usr/bin/env fish
+# No greeting text for now
+set fish_greeting
+
 # Set default editor to vim
 export EDITOR="vim"
 
 # Set some stuff for out path
-export PATH="$PATH $HOME/.local/bin"
+fish_add_path -g ~/.local/bin
 
 # Setup our default FZF command
 export FZF_DEFAULT_COMMAND="rg --files --hidden -g '!.git' | devicon-lookup"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_DEFAULT_OPTS="--height 30%"
-
-# No greeting text for now
-set fish_greeting
 
 # Replace default ls command
 alias ls="exa -F"
@@ -19,3 +20,10 @@ alias ls="exa -F"
 alias l='ls -l'
 alias ll='ls -al'
 alias la='ls -a'
+
+# I want v to open vi and vi to open vim
+alias v=vim
+alias vi=vim
+
+# Alias make to use max cores/threads
+alias make='make -j $(nproc)'
