28ac6f65424c55e170911400558d448a71e4760d
mxaddict <mxaddict@codedmaster.com>
2023-04-13 11:43:27 +0800

    Replaced use of export with set -Ux

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,16 +2,16 @@
 # No greeting text for now
 set fish_greeting
 
-# Set default editor to vim
-export EDITOR="vim"
-
 # Set some stuff for out path
 fish_add_path -g ~/.local/bin
 
+# Set default editor to vim
+set -Ux EDITOR "vim"
+
 # Setup our default FZF command
-export FZF_DEFAULT_COMMAND="rg --files --hidden -g '!.git' | devicon-lookup"
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_DEFAULT_OPTS="--height 30%"
+set -Ux FZF_DEFAULT_COMMAND "rg --files --hidden -g '!.git'"
+set -Ux FZF_CTRL_T_COMMAND  "$FZF_DEFAULT_COMMAND"
+set -Ux FZF_DEFAULT_OPTS    "--height 30%"
 
 # Replace default ls command
 alias ls="exa -F"
@@ -22,8 +22,10 @@ alias ll='ls -al'
 alias la='ls -a'
 
 # I want v to open vi and vi to open vim
-alias v=vim
-alias vi=vim
+alias v="nvim"
+alias vi="nvim"
+alias vim="nvim"
+alias vimdiff="nvim -d"
 
 # Alias make to use max cores/threads
-alias make='make -j $(nproc)'
+alias make='make -j (nproc)'
