a26984720dc06304a3b9d153a5a481cd5bec50ef
mxaddict <mxaddict@codedmaster.com>
2023-04-13 10:27:45 +0800

    Updated fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,21 @@
 # Set default editor to vim
 export EDITOR="vim"
 
+# Set some stuff for out path
+export PATH="$PATH $HOME/.local/bin"
+
 # Setup our default FZF command
 export FZF_DEFAULT_COMMAND="rg --files --hidden -g '!.git' | devicon-lookup"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_DEFAULT_OPTS="--height 30%"
 
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
+# No greeting text for now
+set fish_greeting
 
-alias ls=exa
+# Replace default ls command
+alias ls="exa -F"
+
+# some more ls aliases
+alias l='ls -l'
+alias ll='ls -al'
+alias la='ls -a'
