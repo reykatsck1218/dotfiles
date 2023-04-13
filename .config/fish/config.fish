6e01ab4e5cf343359b4073e42f6b0b47da8a3621
mxaddict <mxaddict@codedmaster.com>
2023-04-13 16:14:56 +0800

    Added more aliases, zoxide, and bat...

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,21 +11,51 @@ set -Ux EDITOR "vim"
 # Setup our default FZF command
 set -Ux FZF_DEFAULT_COMMAND "rg --files --hidden -g '!.git'"
 set -Ux FZF_CTRL_T_COMMAND  "$FZF_DEFAULT_COMMAND"
+set -Ux FZF_CTRL_T_OPTS     "--preview 'bat -n --color=always {}'"
 set -Ux FZF_DEFAULT_OPTS    "--height 30%"
 
+# Use BAT for our man pages
+set -Ux MANPAGER "sh -c 'col -bx | bat -l man -p'"
+
+# Alias for zoxide interactive
+alias z  "z"
+alias zx "zi"
+alias zz "z -"
+
+# Replace cat with batcat
+alias cat "bat --plain"
+alias bathelp='bat --plain --language=help'
+
+function belp
+    $argv --help 2>&1 | bathelp
+end
+
 # Replace default ls command
-alias ls="exa -F"
+alias ls "exa -F"
 
 # Some more ls aliases
-alias l="ls -l"
-alias ll="ls -al"
-alias la="ls -a"
+alias l  "ls -l"
+alias la "ls -a"
+alias ll "ls -al"
 
 # I want v to open vi and vi to open vim
-alias v="nvim"
-alias vi="nvim"
-alias vim="nvim"
-alias vimdiff="nvim -d"
+alias v       "nvim"
+alias vd      "nvim -d"
+alias vi      "nvim"
+alias vim     "nvim"
+alias vimdiff "nvim -d"
 
-# Alias make to use max cores/threads
-alias make="make -j (nproc)"
+# Alias g to git (saves a few key strokes)
+alias g  "git"
+alias gg "git status"
+
+# Alias for make to m (save a few key strokes)
+# as well as make sure to use the max threads available
+alias m    "make -j (nproc)"
+alias make "make -j (nproc)"
+
+# Load fzf
+fzf_key_bindings
+
+# Load zoxide
+zoxide init fish | source
