04483c5d5eddd84ddf7eba26d316b54465e98cb7
mxaddict <mxaddict@codedmaster.com>
2023-04-14 23:24:11 +0800

    Refactored nvim configs

---
+++ .config/nvim/lua/plugins/color.lua
@@ -0,0 +1,9 @@
+return { -- Setup tokyonight options
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+        style = "night",
+        transparent = true,
+    },
+}
