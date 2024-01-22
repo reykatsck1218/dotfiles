f89c4cb69c065005d6a082cb61ba77053c8cf218
mxaddict <mxaddict@codedmaster.com>
2024-01-22 17:29:46 +0800

    Updates

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -69,7 +69,7 @@ require("lazy").setup({
         keys = {
             {
                 mode = { 'v', 'n' },
-                '<Leader>m',
+                '<Leader>n',
                 '<cmd>MCstart<cr>',
                 desc = 'Create a selection for selected text or word under the cursor',
             },
