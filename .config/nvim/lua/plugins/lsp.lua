c4a133df6b39f8b26fa8a06df56f371246244aa9
mxaddict <mxaddict@codedmaster.com>
2024-03-12 00:38:06 +0800

    Updates

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -13,11 +13,12 @@ return { -- LSP Configuration & Plugins
         --  Add any additional override configuration in the following tables. They will be passed to
         --  the `settings` field of the server config. You must look up that documentation yourself.
         local servers = {
-            -- clangd = {},
-            -- gopls = {},
-            -- pyright = {},
+            clangd = {},
+            gopls = {},
+            tsserver = {},
+            pyright = {},
             rust_analyzer = {},
-            -- tsserver = {},
+            intelephense = {},
 
             lua_ls = {
                 Lua = {
