0216e886f48672d4dcc9a5a3920e81ab35847f21
mxaddict <mxaddict@codedmaster.com>
2024-08-02 05:27:27 +0800

    Better nvimtree config

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -4,41 +4,34 @@ return {
     config = function()
         local nvimtree = require("nvim-tree")
 
-        -- recommended settings from nvim-tree documentation
-        vim.g.loaded_netrw = 1
-        vim.g.loaded_netrwPlugin = 1
-
-        -- change color for arrows in tree to light blue
-        vim.cmd([[ highlight NvimTreeFolderArrowClosed guifg=#3FC5FF ]])
-        vim.cmd([[ highlight NvimTreeFolderArrowOpen guifg=#3FC5FF ]])
-
         -- configure nvim-tree
         nvimtree.setup({
+            disable_netrw = true,
+            hijack_cursor = true,
+            sync_root_with_cwd = true,
+            update_focused_file = {
+                enable = true,
+                update_root = false,
+            },
             view = {
                 width = 30,
-                relativenumber = true,
+                preserve_window_proportions = true,
             },
-            -- change folder arrow icons
             renderer = {
-                indent_markers = {
-                    enable = true,
-                },
+                root_folder_label = false,
+                highlight_git = true,
+                indent_markers = { enable = true },
                 icons = {
                     glyphs = {
+                        default = "󰈚",
                         folder = {
-                            arrow_closed = "", -- arrow when folder is closed
-                            arrow_open = "", -- arrow when folder is open
+                            default = "",
+                            empty = "",
+                            empty_open = "",
+                            open = "",
+                            symlink = "",
                         },
-                    },
-                },
-            },
-            -- disable window_picker for
-            -- explorer to work well with
-            -- window splits
-            actions = {
-                open_file = {
-                    window_picker = {
-                        enable = false,
+                        git = { unmerged = "" },
                     },
                 },
             },
@@ -54,8 +47,6 @@ return {
         local keymap = vim.keymap -- for conciseness
 
         keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
-        keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
-        keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
         keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
     end,
 }
