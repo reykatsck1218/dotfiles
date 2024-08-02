091b175c8bdb148474996464fb8fcd328c8a05a8
mxaddict <mxaddict@codedmaster.com>
2024-08-03 07:00:09 +0800

    Added tokyonight theme for fzf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -27,6 +27,31 @@ set -gx FZF_ALT_C_COMMAND "fd --type=d --hidden --exclude .git"
 set -gx FZF_DEFAULT_OPTS "--height 100%"
 set -gx PARU_PAGER "bat --color=always"
 
+# FZF theme
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+    --highlight-line \
+    --info=inline-right \
+    --ansi \
+    --layout=reverse \
+    --border=none
+    --color=bg+:#283457 \
+    --color=bg:#16161e \
+    --color=border:#27a1b9 \
+    --color=fg:#c0caf5 \
+    --color=gutter:#16161e \
+    --color=header:#ff9e64 \
+    --color=hl+:#2ac3de \
+    --color=hl:#2ac3de \
+    --color=info:#545c7e \
+    --color=marker:#ff007c \
+    --color=pointer:#ff007c \
+    --color=prompt:#2ac3de \
+    --color=query:#c0caf5:regular \
+    --color=scrollbar:#27a1b9 \
+    --color=separator:#ff9e64 \
+    --color=spinner:#ff007c \
+    "
+
 # Check for batcat
 if command -v batcat > /dev/null
     alias bat batcat
