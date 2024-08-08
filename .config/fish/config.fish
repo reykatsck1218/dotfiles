2bfe0a383d0a300b913e6a89f5dbc606e5089798
mxaddict <mxaddict@codedmaster.com>
2024-08-08 15:32:12 +0800

    Witness me!!!

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -181,7 +181,7 @@ type -q fzf_configure_bindings && fzf_configure_bindings \
     --directory=\cd \
     --git_log=\ch \
     --git_status=\cg \
-    --variables=\cb \
+    --variables=\cv \
     --processes=\cp
 
 # Load zoxide
