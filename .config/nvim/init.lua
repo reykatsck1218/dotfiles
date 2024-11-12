069fbf317ca37c386ba6cdaea99f7bdcd35eca5a
mxaddict <mxaddict@codedmaster.com>
2024-11-13 06:25:18 +0800

    Moved all the plugins into the init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -126,6 +126,300 @@ require("lazy").setup({
         }
     },
 
+    { -- Autocompletion
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
+            "L3MON4D3/LuaSnip",
+            "saadparwaiz1/cmp_luasnip"
+        },
+        config = function()
+            -- nvim-cmp setup
+            local cmp = require "cmp"
+            local luasnip = require "luasnip"
+
+            luasnip.config.setup {}
+
+            cmp.setup({
+                sources = { {
+                    name = "path",
+                    option = {
+                        trailing_slash = true,
+                        label_trailing_slash = true,
+                    },
+                } },
+            })
+
+            -- `/` cmdline setup.
+            cmp.setup.cmdline("/", {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = {
+                    { name = "buffer" }
+                }
+            })
+
+            -- `:` cmdline setup.
+            cmp.setup.cmdline(":", {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources(
+                    { { name = "path" } },
+                    { {
+                        name = "cmdline",
+                        option = {
+                            ignore_cmds = { "Man", "!" }
+                        }
+                    } }
+                )
+            })
+
+            cmp.setup {
+                snippet = {
+                    expand = function(args)
+                        luasnip.lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert {
+                    ["<c-d>"] = cmp.mapping.scroll_docs(-4),
+                    ["<c-f>"] = cmp.mapping.scroll_docs(4),
+                    ["<c-space>"] = cmp.mapping.complete {},
+                    ["<cr>"] = cmp.mapping.confirm {
+                        behavior = cmp.ConfirmBehavior.Replace,
+                        select = true,
+                    },
+                    ["<tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        elseif luasnip.expand_or_jumpable() then
+                            luasnip.expand_or_jump()
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+                    ["<s-tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        elseif luasnip.jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+                },
+                sources = {
+                    { name = "buffer" },
+                    { name = "luasnip" },
+                    { name = "nvim_lsp" },
+                    { name = "path" },
+                    { name = "vim-dadbod-completion" },
+                },
+            }
+        end
+    },
+
+    { -- Add indentation guides even on blank lines
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        -- See `:help indent_blankline.txt`
+        opts = {
+            indent = { char = "┊" },
+        },
+    },
+
+    { -- Setup tokyonight options
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {
+            style = "night",
+            transparent = false,
+            plugins = { auto = false },
+        },
+    },
+
+    { -- Adds git releated signs to the gutter, as well as utilities for managing changes
+        "lewis6991/gitsigns.nvim",
+        opts = {
+            -- See `:help gitsigns.txt`
+            signs = {
+                add          = { text = "┃" },
+                change       = { text = "┃" },
+                delete       = { text = "_" },
+                topdelete    = { text = "‾" },
+                changedelete = { text = "~" },
+                untracked    = { text = "┆" },
+            },
+            current_line_blame = true,
+        }
+    },
+
+    { -- LSP Configuration & Plugins
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            "j-hui/fidget.nvim",
+            "folke/neodev.nvim", -- Additional lua configuration, makes nvim stuff amazing!
+        },
+        config = function()
+            -- Enable the following language servers
+            --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
+            --
+            --  Add any additional override configuration in the following tables. They will be passed to
+            --  the `settings` field of the server config. You must look up that documentation yourself.
+            local servers = {
+                clangd = {},
+                rust_analyzer = {},
+                intelephense = {},
+                ts_ls = {},
+                pyright = {},
+
+                lua_ls = {
+                    Lua = {
+                        workspace = { checkThirdParty = false },
+                        telemetry = { enable = false },
+                    },
+                },
+            }
+
+            -- Setup neovim lua configuration
+            require("neodev").setup()
+
+            -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+
+            -- Setup mason so it can manage external tooling
+            require("mason").setup()
+
+            -- Ensure the servers above are installed
+            local mason_lspconfig = require "mason-lspconfig"
+
+            mason_lspconfig.setup {
+                ensure_installed = vim.tbl_keys(servers),
+            }
+
+            mason_lspconfig.setup_handlers {
+                function(server_name)
+                    require("lspconfig")[server_name].setup {
+                        capabilities = capabilities,
+                        on_attach = function()
+                            -- NOTE: Remember that lua is a real programming language, and as such it is possible
+                            -- to define small helper and utility functions so you don"t have to repeat yourself
+                            -- many times.
+                            vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename" })
+                            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
+
+                            vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Goto definition" })
+                            vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, { desc = "Goto references" })
+                            vim.keymap.set("n", "gI", vim.lsp.buf.implementation, { desc = "Goto implementation" })
+                            vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, { desc = "Type definition" })
+                            vim.keymap.set("n", "<leader>S", require("telescope.builtin").lsp_document_symbols, { desc = "Document symbols" })
+                            vim.keymap.set("n", "<leader>W", require("telescope.builtin").lsp_dynamic_workspace_symbols,
+                                { desc = "Workspace symbols" })
+
+                            -- See `:help K` for why this keymap
+                            vim.keymap.set("n", "<leader>H", vim.lsp.buf.hover, { desc = "Hover Documentation" })
+                            vim.keymap.set("n", "<leader>S", vim.lsp.buf.signature_help, { desc = "Signature Documentation" })
+
+                            -- Lesser used LSP functionality
+                            vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Goto declaration" })
+                            vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, { desc = "Workspace add folder" })
+                            vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, { desc = "Workspace remove folder" })
+                            vim.keymap.set("n", "<leader>wl", function()
+                                print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                            end, { desc = "Workspace list folders" })
+
+                            -- Format the current buffer with lsp
+                            vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format() end, { desc = "Format buffer" })
+                        end,
+                        settings = servers[server_name],
+                    }
+                end,
+            }
+        end
+    },
+
+    {
+        "nvim-tree/nvim-tree.lua",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            local nvimtree = require("nvim-tree")
+
+            -- configure nvim-tree
+            nvimtree.setup({
+                disable_netrw = true,
+                hijack_cursor = true,
+                sync_root_with_cwd = true,
+                update_focused_file = {
+                    enable = true,
+                    update_root = false,
+                },
+                view = {
+                    width = 30,
+                    preserve_window_proportions = true,
+                },
+                renderer = {
+                    root_folder_label = false,
+                    highlight_git = true,
+                    indent_markers = { enable = true },
+                    icons = {
+                        glyphs = {
+                            default = "󰈚",
+                            folder = {
+                                default = "",
+                                empty = "",
+                                empty_open = "",
+                                open = "",
+                                symlink = "",
+                            },
+                            git = { unmerged = "" },
+                        },
+                    },
+                },
+                filters = {
+                    custom = { ".DS_Store" },
+                },
+                git = {
+                    ignore = false,
+                },
+            })
+
+            -- set keymaps
+            local keymap = vim.keymap -- for conciseness
+
+            keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<cr>", { desc = "Toggle file explorer" }) -- toggle file explorer
+            keymap.set("n", "<leader>E", "<cmd>NvimTreeRefresh<cr>", { desc = "Refresh file explorer" }) -- refresh file explorer
+        end,
+    },
+
+    { -- Set lualine as statusline
+        "nvim-lualine/lualine.nvim",
+        -- See `:help lualine.txt`
+        opts = {
+            options = {
+                icons_enabled = true,
+                theme = "auto",
+                component_separators = "|",
+                section_separators = "",
+            },
+
+            tabline = {
+                lualine_a = { "tabs" },
+            },
+        }
+    },
+
+    { -- Highlight, edit, and navigate code
+        "nvim-treesitter/nvim-treesitter",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter-textobjects",
+        },
+        build = ":TSUpdate",
+    },
+
     { import = "plugins" },
 }, {})
 
