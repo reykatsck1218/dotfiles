22468b469a4908bbbfcf3abea922d8fe193702b4
mxaddict <mxaddict@codedmaster.com>
2024-11-13 06:57:16 +0800

    Updated NOICE configs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -111,6 +111,22 @@ require("lazy").setup({
         "folke/noice.nvim",
         event = "VeryLazy",
         opts = {
+            lsp = {
+                -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+                override = {
+                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                    ["vim.lsp.util.stylize_markdown"] = true,
+                    ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+                },
+            },
+            -- you can enable a preset for easier configuration
+            presets = {
+                bottom_search = false,
+                command_palette = false,
+                long_message_to_split = true,
+                inc_rename = true,
+                lsp_doc_border = false,
+            },
         },
         dependencies = {
             "MunifTanjim/nui.nvim",
